1ced61f501e899969459fe99b8db0a8bf415a0b8
Niv <niv@routier.io>
2021-02-23 01:19:30 +0200

    1. remove the something() 2. add a helper to call protected method

--- tests/Pest.php
+++ tests/Pest.php
@@ -39,7 +39,25 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
+/**
+ * @param $object
+ * @param string $method
+ * @param array $parameters
+ * @return mixed
+ * @throws \Exception
+ */
+function callMethod($object, string $method , array $parameters = [])
 {
-    // ..
+    try {
+        $className = get_class($object);
+        $reflection = new \ReflectionClass($className);
+    }
+    catch (\ReflectionException $e) {
+        throw new \Exception($e->getMessage());
+    }
+
+    $method = $reflection->getMethod($method);
+    $method->setAccessible(true);
+
+    return $method->invokeArgs($object, $parameters);
 }
