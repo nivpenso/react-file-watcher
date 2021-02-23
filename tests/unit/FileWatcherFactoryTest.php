f207153b12bb3f20208239fae01d02496606c726
Niv <niv@routier.io>
2021-02-23 16:56:41 +0200

    remove the usage of uopz extension

--- tests/unit/FileWatcherFactoryTest.php
+++ tests/unit/FileWatcherFactoryTest.php
@@ -1,11 +1,8 @@
 <?php
 
 use React\EventLoop\LoopInterface;
-use React\EventLoop\ExtEventLoop;
-use React\EventLoop\ExtLibeventLoop;
 use React\EventLoop\ExtEvLoop;
 use \React\EventLoop\ExtUvLoop;
-use React\EventLoop\StreamSelectLoop;
 use ReactFileWatcher\Exceptions\FileWatcherLoopNotSupported;
 use ReactFileWatcher\FileWatcherFactory;
 use ReactFileWatcher\Watchers\LibUVFileWatcher;
@@ -23,15 +20,11 @@ it('should return ExtEvFileWatcher when loop implementation is ExtEVLoop',functi
     expect(get_class($fileWatcher))->toBe(LibEVFileWatcher::class);
 })->group("LibUV");
 
-// TODO: need to add into the require-dev of the composer all the ext-packages to allow this test to run.
-it('should throw exception of file watcher not implemented',function ($loopType) {
-    // phpunit doesn't allow creating a mock of a "final" class. Using UOPZ to remove the "final" keyword from the type.
-    /** @noinspection PhpUndefinedFunctionInspection */
-    uopz_flags($loopType, null, 0);
-    $loop = $this->getMockBuilder($loopType)->getMock();
+it('should throw exception of file watcher not implemented when recieve not implemented LoopInterface type',function () {
+    $loop = $this->getMockBuilder(LoopInterface::class)->getMock();
     /** @noinspection PhpParamsInspection */
     FileWatcherFactory::create($loop);
-})->with([StreamSelectLoop::class, ExtLibeventLoop::class,ExtEventLoop::class])->throws(FileWatcherLoopNotSupported::class)->group("StreamSelect");
+})->throws(FileWatcherLoopNotSupported::class);
 
 it('should throw FileWatcherLoopNotSupported when loop implementation is type of unknown LoopInterface',function () {
     $loop = $this->getMockBuilder(LoopInterface::class)->getMock();
