10845b898b0fd617910209762e07bd89a35954f8
Niv <niv@routier.io>
2021-03-07 21:47:13 +0200

    add more functionality to the path watcher based on its members

--- src/PathObjects/PathWatcher.php
+++ src/PathObjects/PathWatcher.php
@@ -2,17 +2,37 @@
 
 namespace ReactFileWatcher\PathObjects;
 
+use Exception;
+use ReactFileWatcher\Exceptions\PathDoesntExist;
+
 class PathWatcher
 {
     protected string $pathToWatch;
     protected bool $isRecursiveWatch;
     protected array $suffixToExclude;
+    protected bool $isDir;
+    protected string $dirPart;
+    protected string $filenamePart;
 
     public function __construct(string $pathToWatch, bool $setRecursiveWatch = true, array $suffixToExclude = [])
     {
         $this->pathToWatch = $pathToWatch;
         $this->isRecursiveWatch = $setRecursiveWatch;
         $this->suffixToExclude = $suffixToExclude;
+        if (is_dir($pathToWatch)) {
+            $this->isDir = true;
+            $this->dirPart = $pathToWatch;
+        }
+        else {
+            if (is_file($pathToWatch)) {
+                $this->isDir = false;
+                $this->filenamePart = basename($this->pathToWatch);
+                $this->dirPart = dirname($this->pathToWatch);
+            }
+            else {
+                throw new PathDoesntExist();
+            }
+        }
     }
 
     /**
@@ -39,4 +59,26 @@ class PathWatcher
         return $this->suffixToExclude;
     }
 
+    /**
+     * @return bool
+     */
+    public function isDir(): bool
+    {
+        return $this->isDir;
+    }
+
+    public function isFile(): bool {
+        return !$this->isDir;
+    }
+
+    public function getDirPart() :string {
+        return $this->dirPart;
+    }
+
+    public function getFilenamePart() :string {
+        if ($this->isDir()) {
+            throw new Exception('Try to get filename on a dir path');
+        }
+        return $this->filenamePart;
+    }
 }
